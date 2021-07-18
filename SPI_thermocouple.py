eb37967e70abede1994bf1da17b4a1217776278e
Emilio Latorre <64216678+elatorre20@users.noreply.github.com>
2021-07-18 17:55:32 -0400

    Finished thermocouple driver
    Finished the first attempt at the driver for reading the thermocouple off the SPI bus. Now starting on some way to log and display those values.

--- SPI_thermocouple.py
+++ SPI_thermocouple.py
@@ -10,7 +10,7 @@ class thermocouple_SPI():
     def __init__(self, bus, cs):
         self.bus = bus
         self.cs = cs
-        self.temp_data = bytearray(2)
+        self.temp_data = 0
         self.cs.value(1)
         
     def select(self):
@@ -19,8 +19,35 @@ class thermocouple_SPI():
     def deselect(self):
         self.cs.value(1)
         
-    def read_temp(self):
+    def refresh(self):
         self.select()
-        self.bus.readinto(self.temp_data)
+        utime.sleep_us(10)
+        self.deselect()
+        
+    def read_temp(self):
+        #the temperature reading is the 2nd-12th bits (bits 14-3 of the read byte)
+        #multiply this number by 0.25 to get the reading in degrees C
+        self.select()
+        utime.sleep_us(10)
+        read = self.bus.read(2)
+        self.deselect()
+        x = int.from_bytes(read, 'big')
+        x = x>>3 #discarding 3 trailing bits
+        mask = 0b0111111111111
+        y = bin(x) & mask #masking leading zero
+        t = 0.25*y
+        self.temp_data = t
         return self.temp_data
+    
+    def get_temp_c(self):
+        return(self.read_temp())
+        
+    def get_temp_k(self):
+        return(self.get_temp_c()-273.15)
+    
+    def get_temp_f(self):
+        return((self.get_temp_c()*1.8)+32)
+    
+    
+        
         
--- controller_v1.py
+++ controller_v1.py
@@ -1,4 +1,4 @@
-from SPI_thermocouple import thermocouple_spi
+import SPI_thermocouple
 import machine
 import utime
 from micropython import const
@@ -19,3 +19,14 @@ CONV_FAN     = machine.Pin(19, Pin.OUT)
 LIGHT_IND    = machine.Pin(18, Pin.OUT)
 
 #setup
+
+spi1 = machine.SPI(1, baudrate=1000000,
+                   polarity=0,
+                   phase=0,
+                   bits=8,
+                   firstbit=SPI.MSB,
+                   sck=SCK, mosi=MOSI, miso=MISO)
+
+t0 = thermocouple_SPI(spi1, T0CS)
+
+
